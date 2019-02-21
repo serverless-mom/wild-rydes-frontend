f6e6d54d221e32246cff4ebee241071e138e4411
Toby Fee <tobyfee@gmail.com>
2019-02-21 13:43:04 -0800

    add file and uploader

--- src/Function/index.js
+++ src/Function/index.js
@@ -1,5 +1,24 @@
-exports.handler = async message => {
-  console.log(message);
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const fileName = "hello.html";
 
-  return {};
-}
+const s3 = new AWS.S3();
+
+module.exports = async event => {
+  console.dir(event);
+  const ports = JSON.parse(process.env.STACKERY_PORTS);
+  let data = fs.readFileSync(`./${fileName}`, "utf8");
+  let params = {
+    Body: data,
+    Key: `${fileName}`,
+    Bucket: ports[0][0].bucket
+  };
+
+  try {
+    await s3.putObject(params);
+  } catch (error) {
+    console.log(err);
+  } finally {
+    return {};
+  }
+};
